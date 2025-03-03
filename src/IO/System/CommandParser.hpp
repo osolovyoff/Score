f5ac378811f85fd04f18a69a01b0040fc91eb6bc
Oleg Solovyoff <osolovyoff@gmail.com>
2025-03-03 01:14:56 -0300

    init commit

---
+++ src/IO/System/CommandParser.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Game/Battle.hpp"
+#include "Game/GameSession.hpp"
+#include "details/CommandParserVisitor.hpp"
+
+#include <functional>
+#include <sstream>
+#include <string>
+
+namespace sw::io
+{
+	class CommandParser
+	{
+	private:
+		std::unordered_map<std::string, std::function<void(std::istream&)>> _commands;
+
+	public:
+		template <class TCommandData>
+		CommandParser& add(std::function<void(TCommandData)> handler)
+		{
+			std::string commandName = TCommandData::Name;
+			auto [it, inserted] = _commands.emplace(
+				commandName,
+				[handler = std::move(handler)
+					](std::istream& stream)
+				{
+					TCommandData data;
+					CommandParserVisitor visitor(stream);
+					data.visit(visitor);
+					GameSession::getInstance().process(data);
+					handler(std::move(data));
+				});
+			if (!inserted)
+			{
+				throw std::runtime_error("Command already exists: " + commandName);
+			}
+
+			return *this;
+		}
+
+		void parse(std::istream& stream);
+	};
+}
